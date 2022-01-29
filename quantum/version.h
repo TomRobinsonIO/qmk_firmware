13708483e5bbacd75e6b9c0f55a4501c1f1ea5cd
TomRobinsonIO <Tom@TomRobinson.io>
2022-01-29 12:58:45 -0500

    first commit

---
+++ quantum/version.h
@@ -0,0 +1,9 @@
+/* This file was automatically generated. Do not edit or copy.
+ */
+
+#pragma once
+
+#define QMK_VERSION "d48bff-dirty"
+#define QMK_BUILDDATE "2022-01-29-17:29:21"
+#define CHIBIOS_VERSION "2022-01-29-17:29:21"
+#define CHIBIOS_CONTRIB_VERSION "2022-01-29-17:29:21"
